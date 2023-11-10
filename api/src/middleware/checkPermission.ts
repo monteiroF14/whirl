9456ae5cda4e7e4054d5912883a8fe62dfbc04f7
monteiroF14 <bluueem@gmail.com>
2023-11-10 19:15:24 +0000

    feat: adding roles to user & config file fix

---
+++ api/src/middleware/checkPermission.ts
@@ -0,0 +1,15 @@
+import type { User } from "@prisma/client";
+import { PERMISSIONS, ROLE_PERMISSIONS } from "@src/config/permissions";
+import type { Request, Response, NextFunction } from "express";
+
+export function checkPermission(permissions: PERMISSIONS[]) {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const { role } = req.body.user as User;
+
+		for (const permission of permissions) {
+			const userHasPermission = ROLE_PERMISSIONS[role].includes(permission);
+
+			return userHasPermission ? next() : res.status(401).json({ error: "Unauthorized" });
+		}
+	};
+}
