b7785dc32a001888fd9327757df98f824ed5d387
monteiroF14 <bluueem@gmail.com>
2023-11-29 01:04:37 +0000

    fix: minor issues

--- api/src/types/express/index.d.ts
+++ api/src/types/express/index.d.ts
@@ -1,12 +1,14 @@
 import type { Quiz } from "../../utils/zod/QuizSchema";
 import type { User } from "../../utils/zod/UserSchema";
 
+// not working
+//  go deeper into it
 declare global {
 	namespace Express {
 		export interface Request {
 			body: {
-				user: User;
-				quiz: Quiz;
+				user?: User;
+				quiz?: Quiz;
 			};
 		}
 	}
