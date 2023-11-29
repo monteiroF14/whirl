1ffcd2863f892379edd63c37044c8263fdcb54d2
monteiroF14 <bluueem@gmail.com>
2023-11-29 01:05:02 +0000

    test: creating a UserControllerMock

--- api/src/config/__mocks__/index.ts
+++ api/src/config/__mocks__/index.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { beforeEach } from "vitest";
 import { mockDeep, mockReset } from "vitest-mock-extended";
+import * as UserController from "../../controllers/user";
 
 export const database = mockDeep<PrismaClient>();
+export const UserControllerMock = mockDeep<typeof UserController>();
 
-beforeEach(() => mockReset(database));
+beforeEach(() => {
+	mockReset(database), mockReset(UserControllerMock);
+});
