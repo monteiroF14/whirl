8be888e4802fede64fba6042c69f35ecd800cf97
monteiroF14 <bluueem@gmail.com>
2023-12-10 02:27:03 +0000

    fix: express rate limit fix?

--- api/src/server.ts
+++ api/src/server.ts
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	validate: { xForwardedForHeader: false },
 	standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Use an external store for consistency across multiple server instances.
